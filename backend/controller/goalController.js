46bcfa483b3f5a885f37022e607bcb78e852ca2f
Mihiraj Wijesooriya <77489538+Omihiraj@users.noreply.github.com>
2022-12-14 12:14:11 +0530

    Connect Db

--- backend/controller/goalController.js
+++ backend/controller/goalController.js
@@ -1,19 +1,40 @@
-const getGoals = (req,res)=>{
-    res.status(200).json({message:"Get Goals"})
-}
-const setGoal = (req,res)=>{
+const asyncHandler = require('express-async-handler')
+const Goal = require('../models/goalModel')
+
+const getGoals = asyncHandler(async(req,res)=>{
+    const goals = await Goal.find()
+    res.status(200).json(goals)
+})
+const setGoal = asyncHandler(async(req,res)=>{
     if(!req.body.text){
         res.status(400)
         throw new Error('Please Add Text Field')
     }
-    res.status(201).json({message:"Set Goal"})
-}
-const updateGoal = (req,res)=>{
-    res.status(200).json({message:`Update Goal ${req.params.id}`})
-}
-const deleteGoal = (req,res)=>{
+    const goal = await Goal.create({
+        text:req.body.text
+    })
+    res.status(201).json(goal)
+})
+const updateGoal = asyncHandler(async(req,res)=>{
+    const goal = await Goal.findById(req.params.id)
+    if(!goal){
+        res.status(400)
+        throw new Error('Goal Not Found')
+    }
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id,req.body,{ 
+      new:true
+    })
+    res.status(200).json(updatedGoal)
+})
+const deleteGoal = asyncHandler(async(req,res)=>{
+    const goal = await Goal.findById(req.params.id)
+    if(!goal){
+        res.status(400)
+        throw new Error('Goal Not Found')
+    }
+    await goal.remove()
     res.status(200).json({message:`Delete Goal ${req.params.id}`})
-}
+})
 
 
 module.exports = {
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv').config()
 const {errorHandler} = require('./middleware/errorMiddleware')
-
+const connectDB = require('./config/db')
+const colors = require('colors')
 const port = process.env.PORT || 5000
+
+
+connectDB()
+
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use('/api/goals',require('./routes/getRoutes'))
 app.use(errorHandler)
-
 
 
 app.listen(port,()=>console.log(`Server Start on PORT ${port}`))
